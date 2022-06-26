e71a0c23b75bf14297b6480756d9d8fdf293a1e6
kenalarya <106664574+kenalarya@users.noreply.github.com>
2022-06-26 17:39:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ["6282351864095","6282351864095","6288215743467,"] //ur owner number
+global.owner = ["6288215743467","6288215743467","6288215743467,"] //ur owner number
 global.ownername = "Arya."
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
